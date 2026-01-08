b31ef56a60a0fb7b12728ad08692a95494a64a26
papertimey-prog <papertimey@icloud.com>
2026-01-08 19:52:25 +0300

    DS
    Signed-off-by: papertimey-prog <papertimey@icloud.com>

---
+++ web.js/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.sudo.app",
+  appName: "web.js",
+  webDir: "dist",
+};
+
+export default config;
